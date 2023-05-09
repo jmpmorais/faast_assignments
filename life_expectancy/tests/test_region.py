b2a3bb16e3c309eacd09e1bc85d64ed014e69456
Joao Prata Morais <joao.morais@nos.pt>
2023-05-09 15:33:46 +0100

    lint region

--- life_expectancy/tests/test_region.py
+++ life_expectancy/tests/test_region.py
@@ -1,12 +1,11 @@
 """Tests for the region enum"""
+from typing import List
 import numpy as np
-
 from life_expectancy.region import Region
 
-def test_regions_list(all_regions_expected):
-    """Test for the region listing function"""
 
+def test_regions_list(all_regions_expected: List[str]):
+    """Test for the region listing function"""
+    
     region_list = Region.list_countries()
-    np.testing.assert_array_equal(
-        region_list, all_regions_expected
-    )
+    np.testing.assert_array_equal(region_list, all_regions_expected)
